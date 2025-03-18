d78b0e99b46a7d9f0c5b05ee382b51db73289dff
Lithium Valproate <minaki@matsu.rip>
2025-03-18 10:41:38 +0800

    tg-0fix

--- C++/XJU/Cpp_T_0.cpp
+++ C++/XJU/Cpp_T_0.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 using namespace std;
-#define PRINT(a, b)                         \
-    cout << a << "+" << b << "i" << endl;
+#define PRINT(a, b)                           \
+    if (b > 0)                                \
+        cout << a << "+" << b << "i" << endl; \
+    else if (b == 0)                          \
+        cout << a << endl;                    \
+    else                                      \
+        cout << a << b << "i" << endl;
 struct Complex {
     int real;
-    int imaginary;
+    int imag;
 };
 void print_complex(Complex z) {
-    PRINT(z.real, z.imaginary);
+    PRINT(z.real, z.imag);
 }
 void add(Complex a, Complex b) {
     int r = a.real + b.real;
-    int i = a.imaginary + b.imaginary;
+    int i = a.imag + b.imag;
     PRINT(r, i);
 }
 int main() {
     Complex x, y;
-    cin >> x.real >> x.imaginary >> y.real >> y.imaginary;
+    cin >> x.real >> x.imag >> y.real >> y.imag;
     print_complex(x);
     print_complex(y);
     add(x, y);
